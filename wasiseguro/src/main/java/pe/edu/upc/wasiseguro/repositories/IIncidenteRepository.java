013c697894241fca940380bb5194269e8eafba4f
user1millon <leonelizzetti@gmail.com>
2026-05-09 03:39:40 -0500

    feat: Correccion de errores

--- wasiseguro/src/main/java/pe/edu/upc/wasiseguro/repositories/IIncidenteRepository.java
+++ wasiseguro/src/main/java/pe/edu/upc/wasiseguro/repositories/IIncidenteRepository.java
@@ -2,35 +2,19 @@ package pe.edu.upc.wasiseguro.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.wasiseguro.dtos.IncidenteCantidadDTO;
 import pe.edu.upc.wasiseguro.entities.Incidente;
-
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IIncidenteRepository extends JpaRepository<Incidente, UUID> {
+    List<Incidente> findAllByOrderByFechaOcurridoDesc();
+    List<Incidente> findByEstado(String estado);
+    List<Incidente> findByTipoIncidenteNombre(String nombreTipo);
+    List<Incidente> findByVotosValidoGreaterThan(int minVotos);
 
-    @Query("SELECT i FROM Incidente i WHERE i.estado = :estado")
-    List<Incidente> buscarPorEstado(@Param("estado") String estado);
-
-    @Query("SELECT i FROM Incidente i WHERE i.tipoIncidente.nombre = :nombreTipo")
-    List<Incidente> buscarPorTipo(@Param("nombreTipo") String nombreTipo);
-
-    @Query("SELECT i FROM Incidente i WHERE i.votosValido > :minVotos")
-    List<Incidente> buscarIncidentesMuyReportados(@Param("minVotos") int minVotos);
-
-    @Query("SELECT new pe.edu.upc.wasiseguro.dtos.IncidenteCantidadDTO(i.tipoIncidente.nombre, COUNT(i)) " +
-            "FROM Incidente i GROUP BY i.tipoIncidente.nombre")
+    @Query("SELECT new pe.edu.upc.wasiseguro.dtos.IncidenteCantidadDTO(i.tipoIncidente.nombre, COUNT(i)) FROM Incidente i GROUP BY i.tipoIncidente.nombre")
     List<IncidenteCantidadDTO> countIncidentesByType();
-
-    @Query("SELECT new pe.edu.upc.wasiseguro.dtos.IncidenteCantidadDTO(i.usuario.nombre, COUNT(i)) " +
-            "FROM Incidente i " +
-            "GROUP BY i.usuario.nombre")
-    List<IncidenteCantidadDTO> reportePorUsuario();
-
-    @Query("SELECT i FROM Incidente i ORDER BY i.fechaOcurrido DESC")
-    List<Incidente> listarTodoOrdenado();
 }
