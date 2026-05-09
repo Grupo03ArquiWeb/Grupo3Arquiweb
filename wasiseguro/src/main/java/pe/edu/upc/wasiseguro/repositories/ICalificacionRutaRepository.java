a36276b5645b2fb00dd5ce6aaf266d69a6e0f95f
sebass09 <isaac.sebas15@gmail.com>
2026-05-09 04:52:23 -0500

    feat: US finalizadas

--- wasiseguro/src/main/java/pe/edu/upc/wasiseguro/repositories/ICalificacionRutaRepository.java
+++ wasiseguro/src/main/java/pe/edu/upc/wasiseguro/repositories/ICalificacionRutaRepository.java
@@ -5,14 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.wasiseguro.dtos.RutaSeguridadDTO;
 import pe.edu.upc.wasiseguro.entities.CalificacionRuta;
-
 import java.util.List;
 
 @Repository
 public interface ICalificacionRutaRepository extends JpaRepository<CalificacionRuta, Integer> {
-
-    @Query(value = "SELECT r.nombre_destino as nombreDestino, AVG(c.puntaje) as promedioSeguridad " +
-            "FROM calificacion_ruta c JOIN ruta r ON c.id_ruta = r.id " +
-            "GROUP BY r.nombre_destino", nativeQuery = true)
+    @Query("SELECT r.nombreDestino as nombreDestino, AVG(c.puntaje) as promedioSeguridad " +
+            "FROM CalificacionRuta c JOIN c.ruta r " +
+            "GROUP BY r.nombreDestino")
     List<RutaSeguridadDTO> getPromedioSeguridadPorRuta();
 }
--- wasiseguro/src/main/java/pe/edu/upc/wasiseguro/servicesimplements/CalificacionRutaServiceImplement.java
+++ wasiseguro/src/main/java/pe/edu/upc/wasiseguro/servicesimplements/CalificacionRutaServiceImplement.java
@@ -6,25 +6,16 @@ import pe.edu.upc.wasiseguro.dtos.RutaSeguridadDTO;
 import pe.edu.upc.wasiseguro.entities.CalificacionRuta;
 import pe.edu.upc.wasiseguro.repositories.ICalificacionRutaRepository;
 import pe.edu.upc.wasiseguro.servicesinterfaces.ICalificacionRutaService;
-
 import java.util.List;
 
 @Service
 public class CalificacionRutaServiceImplement implements ICalificacionRutaService {
     @Autowired
     private ICalificacionRutaRepository cR;
-
     @Override
-    public void insert(CalificacionRuta calificacionRuta) {
-        cR.save(calificacionRuta);
-    }
-
+    public void insert(CalificacionRuta calificacionRuta) { cR.save(calificacionRuta); }
     @Override
-    public List<CalificacionRuta> list() {
-        return cR.findAll();
-    }
+    public List<CalificacionRuta> list() { return cR.findAll(); }
     @Override
-    public List<RutaSeguridadDTO> obtenerPromedioSeguridad() {
-        return cR.getPromedioSeguridadPorRuta();
-    }
+    public List<RutaSeguridadDTO> obtenerPromedioSeguridad() { return cR.getPromedioSeguridadPorRuta(); }
 }
